9d33271ac27243ae815f324e48d9a329ddf763a7
Mingxuan <mchi5@jhu.edu>
2022-10-07 14:21:19 -0400

    update

--- week4-homework/Q6.py
+++ week4-homework/Q6.py
@@ -6,19 +6,19 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-# vcf_fn = sys.argv[2]
-vcf_fn = 'gwas_data/genotypes.vcf'
-# SNP = sys.argv[1]
-SNP = 'rs10876043'
-# pheno_fn = sys.argv[3]
-pheno_fn = 'gwas_data/CB1908_IC50.txt'
+vcf_fn = sys.argv[2]
+# vcf_fn = 'gwas_data/genotypes.vcf'
+SNP = sys.argv[1]
+# SNP = 'rs10876043'
+pheno_fn = sys.argv[3]
+# pheno_fn = 'gwas_data/CB1908_IC50.txt'
 
 intersted_SNP_info_list = list()
 for line in open(vcf_fn):
 	if line.startswith('#') and line[1] != '#':
 		header = line.strip('\n').split('\t')[9::]
 	if SNP in line:
-		print(line)
+		# print(line)
 		for item in line.strip('\n').split('\t'):
 			if item != '':
 				intersted_SNP_info_list.append(item)
@@ -66,8 +66,8 @@ ax.set_xticks([1, 2, 3], count_dict.keys())
 ax.set_ylabel('Phenotype IC50')
 ax.set_xlabel('Genotypes')
 ax.set_title('IC50 of Different Genotypes')
-plt.show()
+# plt.show()
 fig.savefig('Q6.png')
 plt.close(fig)
 
-print(ax_dict['means'])
+# print(ax_dict['means'])
